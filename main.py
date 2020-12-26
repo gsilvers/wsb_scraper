ed7e1de55f3542482eb8f96b02883b9ac9fa789e
Greg Silverstein <greg.silverstein@gmail.com>
2020-12-26 14:29:20 -0500

    Minor Refactor Var Names

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 
+'''
+    Hi u/TiltedOneTrick 
+    I've added some code changes but also some comments to help explain why 
+    overall this code is really good and these changes are just things to learn
+    for your next project. Please give a shout if you need anything or have q's
+
+    Things I've Changed and Why
+    1 - You have some inconsistancies and issues with your namings nothing big but for 
+        example you have client_id which is snake case but files which is not 
+        there are guides for python like https://www.python.org/dev/peps/pep-0008/
+        but in general just stick with the same fully through your code if you're 
+        uncertain always go with word_word in python for functions and variables
+        if you are to the point of making classes and instances then you use some
+        CapitolCase.
+    2 - Some of your variable names you say like _list or _dict. This is good to do
+        if you make your own custom class but you don't need to say its a dict or list 
+    3 - Not sure if this is just because or what but you have these three ticker files 
+        list1,2,3 you seem to be just reading them so i renamed the file and made it one
+    4 - You are on the right path putting get_tickers as its own function 
+        I am guessing you picked that up front. That is good but once you have your code
+        working you should "re-factor" and see if you can clean up your code to do the
+        same thing but more clearly or faster this is a huge part of coding if you
+        take it up as a job I am refactoring your code to break things into a few more functions
+        function 1 : get_ticker
+'''
+
 import praw
 import re
 import pandas as pd
 import config
+
+
 def get_tickers():
     reddit = praw.Reddit(
         client_id= config.api_id,
@@ -17,14 +45,14 @@ def get_tickers():
     weekly_tickers = {}
     regex_pattern = r'\b([A-Z]+)\b'
     phrases = {}
-    ticker_dict = {}
-    filelist = ["list1.csv", "list2.csv", "list3.csv"]
-    for file in filelist:
+    tickers = {}
+    files = ["list1.csv", "list2.csv", "list3.csv"]
+    for file in files:
         tl = pd.read_csv(file, skiprows=0, skip_blank_lines=True)
         tl = tl[tl.columns[0]].tolist()
         for ticker in tl:
-            ticker_dict[ticker] = 1
-    blacklist = ["A", "I", "DD", "WSB", "YOLO", "RH"]
+            tickers[ticker] = 1
+    black_list = ["A", "I", "DD", "WSB", "YOLO", "RH"]
     for submission in reddit.subreddit("wallstreetbets").top("week"):
         strings = [submission.title]
         submission.comments.replace_more(limit=0)
@@ -32,8 +60,8 @@ def get_tickers():
             strings.append(comment.body)
         for s in strings:
             for phrase in re.findall(regex_pattern, s):
-                if phrase not in blacklist:
-                    if ticker_dict.get(phrase) == 1:
+                if phrase not in black_list:
+                    if tickers.get(phrase) == 1:
                         if weekly_tickers.get(phrase) is None:
                             weekly_tickers[phrase] = 1
                         else:
